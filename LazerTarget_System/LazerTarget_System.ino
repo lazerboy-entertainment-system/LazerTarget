11b2f764ca92dda7f01fc879b312ab6cb8ae7095
kradylov@gmail.com <kradylov@gmail.com>
2019-04-24 13:32:25 -0600

    fixed with switch button bug, implemented function isRoomTOoBright, and game mode0

--- LazerTarget_System/LazerTarget_System.ino
+++ LazerTarget_System/LazerTarget_System.ino
@@ -14,8 +14,11 @@
 // INCLUDES
 #include <Arduino.h>
 
+
 #include "talkie.h"
+#include "FastLED.h"
 #include "vocab.h"
+#include "game_mode.h"
 
 // CONSTANT DEFINITIONS
 #define PIN_SPEAKER       3
@@ -26,10 +29,9 @@
 
 #define PIN_GDM_LDR       14    // THIS IS ANALOG PIN A0
 
-
 #define PIN_SWITCH_BTN    2     // button for switching between two game mode. 
-
-#define LUX_THRESHOLD_GDM_LDR   512
+#define PIN_LED           6
+#define LUX_THRESHOLD_GDM_LDR   280
 
 #define LED_BLINK_DELAY   80
 #define LED_BLINK_CYCLES  5
@@ -38,9 +40,9 @@
 #define CPU_MHZ                       16
 #define TIMER_PRESCALAR               1024
 
+#define NUMBER_OF_GDM_LDRS 6
 
-
-#define BTN_DEBOUNCE_TIME 50
+#define NUM_LEDS 1
 #define NUMBER_OF_GP_TIMERS 8
 
 // ------------------------------------------------------------------------------------------------------
@@ -61,13 +63,20 @@ struct timer32_t
 // GLOBAL CONSTANTS
 const double MAX_TIMER_ISR_COUNT = ((CPU_MHZ * 1000.0) / TIMER_PRESCALAR * TIMER_INTERVAL_MILLISECONDS) - 1;
 
-
-
+#define BTN_DEBOUNCE_TIME 15   // debounce time whose value is 100ms
 
 // array of eight general purpose timer32_t records
 volatile timer32_t timer_gpArray[8] = {0, 0, 31, 0};
+volatile timer32_t timer_btnDebounce = {false, 0, BTN_DEBOUNCE_TIME , false};
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
+volatile bool flag_isBtnEnabled = false;
+volatile int game_mode = 0;
+volatile bool flag_activateGameModeVoice = false;
+volatile bool flag_targetHit = false;
+volatile unsigned long lastTime = millis();                       // store current time in milliseconds after the program starts
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
 // FORWARD DECLARATIONS:
 
 bool isTargetHit();     // RETURN TRUE IF THE TARGET HAS BEEN HIT
@@ -78,20 +87,8 @@ void blinkBlueLED();    // BLINKS THE BLUE LED
 
 bool isRoomTooBright();  // RETURN TRUE IF ROOM IS TOO BRIGHT FOR PLAYING THE GAME
 
-void timer_start(int timerNumber);                              // sets enabled flag for gp timer
-void timer_stop(int timerNumber);                               // clears enabled flag for gp timer
-void timer_setCount(int timerNumber, uint32_t count );          // sets count
-void timer_setMaxCount(int timerNumber, uint32_t maxCount);    // sets maxCount
-void timer_setDoEvent(int timerNumber);                        // sets doEvent flag
-void timer_clearDoEvent(int timerNumber);                     // clears doEvent flag
-bool timer_isActive(int timerNumber);                         // returns true if timer is enabled and greater than 0
-
-
-
-volatile timer32_t timer_btnDebounce = {0, 0, BTN_DEBOUNCE_TIME , 0};
-
-volatile bool flag_isBtnPressed = false;
-volatile bool flag_isBtnEnabled = true;
+// -----------------------------------------------------------------------------------------------------------------------------------------
+CRGB leds[NUM_LEDS];
 
 
 // PROGRAM INITIALIZATION
@@ -105,52 +102,69 @@ void setup()
   pinMode(PIN_SPEAKER, OUTPUT);
   pinMode(PIN_SWITCH_BTN, INPUT_PULLUP);                 // D2 input mode with pull-up resistor
 
+
+  FastLED.addLeds<WS2812, PIN_LED, RGB>(leds, NUM_LEDS);
+// FastLED.setBrightness(1);
+
+  // DISABLE INTERRUPTS
+  cli();
+
   // attach an interrupt on int0 and call ISR_BTN_PRESSED() when triggered
   attachInterrupt(digitalPinToInterrupt(PIN_SWITCH_BTN), ISR_BTN_PRESSED, FALLING);
 
 
   // CLEAR TIMER/COUNTER REGISTERS
-  TCCR2A = 0;
-  TCCR2B = 0;
-  TCNT2  = 0;
+  TCCR0A = 0;
+  TCCR0B = 0;
+  TCNT0  = 0;
 
-  // SET MAX TIMER COUNTER
-  OCR2A = MAX_TIMER_ISR_COUNT;
+  // SET MAX TIMER COUNTER    Serial.print("mode: ");
+  Serial.println(game_mode);
+  OCR0A = MAX_TIMER_ISR_COUNT;
 
   // CLEAR TIMER ON COMPARE MATCH
-  TCCR2B |= (1 << WGM12);
+  TCCR0B |= (1 << WGM12);
 
   // SET CS12 AND CS10 TO USE PRESCALAR 1024
-  TCCR2B |= (1 << CS12);
-  TCCR2B |= (1 << CS10);
+  TCCR0B |= (1 << CS12);
+  TCCR0B |= (1 << CS10);
 
   // ENBABLE TIMER COMPARE INTERRUPT
-  TIMSK2 |= (1 << OCIE2A);
+  TIMSK0 |= (1 << OCIE0A);
 
   // ENABLE INTERRUPTS
   sei();
 
+
+
   voice.say(spINTRO);
 
+  game_mode = -1;
+  flag_activateGameModeVoice = false;
+  flag_isBtnEnabled = true;
+
+  Serial.print("mode: ");
+  Serial.println(game_mode);
+
   Serial.println("FINISHED INITIALIZING TARGET");
-  flag_isBtnPressed = false;
 }
-
-
-
 
 // MAIN PROGRAM LOOP
 void loop()
 {
 
-  if (flag_isBtnPressed) {
-    Serial.println("Push pressed.. ");
-    flag_isBtnPressed = false;
-  }
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(1000);
 
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(1000);
+
+
+  
   if (timer_btnDebounce.flag_doEvent)  {
-
-    if (digitalRead(PIN_SWITCH_BTN) == LOW) {
+    if (digitalRead(PIN_SWITCH_BTN) == HIGH) {
       timer_btnDebounce.flag_doEvent = false;
       timer_btnDebounce.count = BTN_DEBOUNCE_TIME;
       flag_isBtnEnabled = true;
@@ -158,46 +172,92 @@ void loop()
   }
 
 
+  // It notifies the player of current mode once after the switch btn is pressed
+  // Then, one of the game types will be played according to the game mode
+  switch (game_mode) {
+    case GAME0: {                                     // Target Mode
 
-  if (isTargetHit())
-  {
-    // INDICATE HIT OCCURRED
+        Serial.println("Game Mode0....");
+        if (flag_activateGameModeVoice)
+          voice.say(spREADY, false);
 
-
-    // OUTPUT SPEECH FOR HIT EVENT
-    // NOTE: USE 'false' ARGUMENT FOR NON-BLOCKING SPEECH
-    voice.say(spHIT, false);
-    // voice.say(spGREAT2, false);
-
-
-    // OUTPUT MESSAGE TO SERIAL CONSOLE
-    Serial.println("HIT");
+        flag_activateGameModeVoice = false;
 
 
-    // BLINK LED
-    blinkGreenLED();
+        if (isTargetHit())
+        {
+          // INDICATE HIT OCCURRED
+
+          // OUTPUT SPEECH FOR HIT EVENT
+          // NOTE: USE 'false' ARGUMENT FOR NON-BLOCKING SPEECH
+          voice.say(spHIT, false);
+
+          // OUTPUT MESSAGE TO SERIAL CONSOLE
+          Serial.println("HIT");
+
+          // BLINK LED
+          //blinkGreenLED();
 
 
-    // END IF
+
+        }       // END IF
+        break;
+      }
+
+    case GAME1: {                                     // Duck Duck game mode                                         
+
+        Serial.println("Game Mode1....");
+
+        if (flag_activateGameModeVoice)
+          voice.say(spREADY);
+
+        flag_activateGameModeVoice = false;
+
+        break;
+      }
+
+    case GAME2: {                                     // Director Mode?
+
+        Serial.println("Game Mode2....");
+        if (flag_activateGameModeVoice)
+          voice.say(spREADY);
+
+        flag_activateGameModeVoice = false;
+        break;
+      }
+
+//          default:
+//            Serial.println("Game Mode DEFAULT....");
+//            voice.say(spGREAT2);
+//            game_mode = -1;
   }
 
 
 }
 
-// activates debounce timer once the push button is pressed
+// activates debounce timer and switches to the next game mode once the push button is pressed
+// In addition, when the button is pressed in order to switch to consequence game mode,
+// the voice will inform the player of what game mode is activated
 void ISR_BTN_PRESSED() {
+
 
   if (flag_isBtnEnabled) {
     flag_isBtnEnabled = false;
 
     timer_btnDebounce.flag_isEnabled = true;
-    flag_isBtnPressed = true;
+
+    game_mode += 1;
+
+    flag_activateGameModeVoice = true;
+
+
   }
+
 }
 
 
 // INTERRUPT SERVICE ROUTINE FOR TIMER1
-ISR(TIMER2_COMPA_vect)
+ISR(TIMER0_COMPA_vect)
 {
 
   if (timer_btnDebounce.flag_isEnabled) {
@@ -210,23 +270,25 @@ ISR(TIMER2_COMPA_vect)
       --timer_btnDebounce.count;
   }
 
-
+  //  if (isTargetHit()) {
+  //    flag_targetHit = true;
+  //   // Serial.println("HIT00000");
+  //  }
 }
 
 // sets enabled flag for gp timer
-void timer_start(int timerNumber) {
+void timer_start(uint32_t timerNumber) {
   timer_gpArray[timerNumber].flag_isEnabled = true;
-
 }
 
 // clears enabled flag for gp timer
-void timer_stop(int timerNumber) {
+void timer_stop(uint32_t timerNumber) {
   timer_gpArray[timerNumber].flag_isEnabled = false;
 
 }
 
 // sets count
-void timer_setCount(int timerNumber, uint32_t count) {
+void timer_setCount(uint32_t timerNumber, uint32_t count) {
   timer_gpArray[timerNumber].count = count;
 }
 
@@ -236,18 +298,18 @@ void timer_setMaxCount(int timerNumber, uint32_t maxCount) {
 }
 
 // sets doEvent flag
-void timer_setDoEvent(int timerNumber) {
+void timer_setDoEvent(uint32_t timerNumber) {
   timer_gpArray[timerNumber].flag_doEvent = true;
 }
 
 
 // clears doEvent flag
-void timer_clearDoEvent(int timerNumber) {
+void timer_clearDoEvent(uint32_t timerNumber) {
   timer_gpArray[timerNumber].flag_doEvent = false;
 }
 
 // returns true if timer is enabled and greater than 0
-bool timer_isActive(int timerNumber) {
+bool timer_isActive(uint32_t timerNumber) {
 
   if (timer_gpArray[timerNumber].flag_isEnabled && timer_gpArray[timerNumber].count > 0)
     return true;
@@ -256,40 +318,31 @@ bool timer_isActive(int timerNumber) {
 
 }
 
-
-
 // RETURN TRUE IF THE TARGET HAS BEEN HIT
-bool isTargetHit()
-{
-  bool result = false;
+bool isTargetHit() {
 
-  // READ ANALOG PIN 6 TIMES TO EMULATE FULL PROTOTYPE TIMING
-  if (analogRead(PIN_GDM_LDR) >= LUX_THRESHOLD_GDM_LDR)
-  {
-    result = true;
-  }
-  if (analogRead(PIN_GDM_LDR) >= LUX_THRESHOLD_GDM_LDR)
-  {
-    result = true;
-  }
-  if (analogRead(PIN_GDM_LDR) >= LUX_THRESHOLD_GDM_LDR)
-  {
-    result = true;
-  }
-  if (analogRead(PIN_GDM_LDR) >= LUX_THRESHOLD_GDM_LDR)
-  {
-    result = true;
-  }
-  if (analogRead(PIN_GDM_LDR) >= LUX_THRESHOLD_GDM_LDR)
-  {
-    result = true;
-  }
-  if (analogRead(PIN_GDM_LDR) >= LUX_THRESHOLD_GDM_LDR)
-  {
-    result = true;
-  }
+  if (analogRead(0) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
 
-  return result;
+  if (analogRead(1) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
+
+  if (analogRead(2) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
+
+  if (analogRead(3) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
+
+  if (analogRead(4) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
+
+  if (analogRead(5) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
+
+  if (analogRead(6) >= LUX_THRESHOLD_GDM_LDR)
+    return true;
+
+  return false;
 }
 
 
@@ -320,8 +373,8 @@ void blinkGreenLED()
 }
 
 // BLINKS THE BLUE LED
-void blinkBlueLED()
-{
+void blinkBlueLED() {
+
   // BLINK LED
   for (int i = 0; i < LED_BLINK_CYCLES; ++i)
   {
@@ -330,4 +383,51 @@ void blinkBlueLED()
     digitalWrite(PIN_LED_RED, LOW);
     delay(LED_BLINK_DELAY);
   }
+}
+
+
+
+bool isRoomTooBright() {
+
+  bool flag = false;
+
+  int LDR_register = 0;
+
+  for (int i = 0; i < NUMBER_OF_GDM_LDRS; ++i) {
+    if (analogRead(i) >= LUX_THRESHOLD_GDM_LDR) {
+      LDR_register |= (LDR_register << (i + 1));
+    }
+  }
+
+  //  if (analogRead(0) >= LUX_THRESHOLD_GDM_LDR)
+  //    LDR_register |= B00000001;
+  //
+  //  if (analogRead(1) >= LUX_THRESHOLD_GDM_LDR)
+  //    LDR_register |= B00000010;
+  //
+  //  if (analogRead(2) >= LUX_THRESHOLD_GDM_LDR)
+  //    LDR_register |= B00000100;
+  //
+  //  if (analogRead(3) >= LUX_THRESHOLD_GDM_LDR)
+  //    LDR_register |= B00001000;
+  //
+  //  if (analogRead(4) >= LUX_THRESHOLD_GDM_LDR)
+  //    LDR_register |= B00010000;
+  //
+  //  if (analogRead(5) >= LUX_THRESHOLD_GDM_LDR)
+  //    LDR_register |= B00100000;
+
+
+
+  if (LDR_register == B00111111)
+  {
+    voice.say(spROOMS_TOO_BRIGHT);
+    Serial.println("ROOM IS TOO BRIGHT");
+    flag = true;
+  }
+
+
+  LDR_register = 0;
+
+  return flag;
 }
---
+++ LazerTarget_System/game_mode.h
@@ -0,0 +1,7 @@
+#define GAME0 0
+#define GAME1 1
+#define GAME2 2
+#define GAME3 3
+#define GAME4 4
+#define GAME5 5
+#define GAME6 6
